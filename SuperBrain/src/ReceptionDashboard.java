fe2840a8cb49ab2c9095ba6243ab6ca7d680f739
sansharma <sandesh.sharma@deerwalk.edu.np>
2017-07-10 23:26:30 -0700

    Changed the name of button

--- SuperBrain/src/ReceptionDashboard.java
+++ SuperBrain/src/ReceptionDashboard.java
@@ -1,13 +1,15 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * Created by Dell on 7/9/2017.
  */
 public class ReceptionDashboard extends JFrame{
     private JPanel panel=new JPanel();
-    private JButton button=new JButton("StudentEnroll");
-    private JButton button2=new JButton("CourseDetails");
+    private JButton studentEnrollButton=new JButton("StudentEnroll");
+    private JButton courseDetailbutton=new JButton("CourseDetails");
 
 
     public ReceptionDashboard(){
@@ -15,24 +17,38 @@ public class ReceptionDashboard extends JFrame{
 
         GridBagConstraints gbc =new GridBagConstraints();
         panel.setLayout(gridBagLayout);
-        button=new JButton("STUDENTS ENROLL");
+        studentEnrollButton=new JButton("STUDENTS ENROLL");
         gbc.gridx=2;
         gbc.gridy=1;
         gbc.ipadx = 5;
         gbc.ipady = 2;
         gbc.gridwidth = 4;
         gbc.fill = GridBagConstraints.HORIZONTAL;
-        panel.add(button,gbc);
-        button2=new JButton("COURSE DETAILS");
+        panel.add(studentEnrollButton,gbc);
+        courseDetailbutton=new JButton("COURSE DETAILS");
         gbc.gridwidth = 1;
         gbc.gridx=2;
         gbc.gridy=7;
         gbc.ipadx = 6;
         gbc.ipady = 3;
-        panel.add(button2,gbc);
+        panel.add(courseDetailbutton,gbc);
         add(panel);
         setVisible(true);
         setSize(300,300);
+
+        studentEnrollButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new StudentEnroll();
+            }
+        });
+
+        courseDetailbutton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                    new CourseDetail();
+            }
+        });
     }
 
     public static void main(String args []){
