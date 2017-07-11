28c4905866091f16b76daadecc468abff4169ec8
sansharma <sandesh.sharma@deerwalk.edu.np>
2017-07-10 23:28:50 -0700

    Created Class CourseDetail

---
+++ SuperBrain/src/CourseDetail.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+/**
+ * Created by Sandesh on 7/10/2017.
+ */
+public class CourseDetail extends JFrame {
+    public CourseDetail(){
+        setVisible(true);
+        setSize(300,300);
+
+
+
+    }
+
+}
