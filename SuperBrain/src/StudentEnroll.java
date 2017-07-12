2edf456b37d48145a2e41fb33a8bb5ffed09bddc
sansharma <sandesh.sharma@deerwalk.edu.np>
2017-07-11 22:26:00 -0700

    Changed the design of StudentEnroll Frame

--- SuperBrain/src/StudentEnroll.java
+++ SuperBrain/src/StudentEnroll.java
@@ -5,90 +5,102 @@ import java.awt.*;
  * Created by Dell on 7/11/2017.
  */
 public class StudentEnroll extends JFrame {
-    private JPanel Panel;
-    private JLabel label=new JLabel();
-    private JButton studentEnrollbutton;
-    private JTextField Name,Age,Address,Sex,ContactNo,Course;
-    private JLabel jName,jAge,jAddress,jSex,jContactNo,jCourse;
-    private JLabel titleLabel=new JLabel();
-    private JPanel panel=new JPanel();
-    private JTextField name = new JTextField();
-    private JTextField age = new JTextField();
-    private JTextField address = new JTextField();
-    private JTextField sex = new JTextField();
-    private JTextField contactno = new JTextField();
-    private JTextField course = new JTextField();
+    private JPanel panel;
+    private JLabel label;
+    private JButton submitButton;
+    private JLabel nameLabel;
+    private JLabel ageLabel;
+    private JLabel addressLabel;
+    private JLabel sexLabel;
+    private JLabel contactLabel;
+    private JLabel courseLabel;
+    private JLabel titleLabel;
+    private JTextField name;
+    private JTextField age;
+    private JTextField address;
+    private JTextField sex ;
+    private JTextField contactno;
+    private JTextField course;
+
 
 
     public StudentEnroll(){
+        panel = new JPanel();
         GridBagLayout gridBagLayout=new GridBagLayout();
         GridBagConstraints gbc =new GridBagConstraints();
-        gbc.gridx = 4;
-        gbc.gridy = 0;
-
-        gbc.weightx = 1;
-        gbc.weighty = 1;
         panel.setLayout(gridBagLayout);
 
-        titleLabel=new JLabel("Name");
+
+        nameLabel =new JLabel("Name");
         gbc.gridx=0;
         gbc.gridy=0;
-        panel.add(titleLabel,gbc);
+        panel.add(nameLabel,gbc);
+
+        addressLabel = new JLabel("Address");
+        gbc.gridy++;
+        gbc.gridy++;
+        panel.add(addressLabel,gbc);
+
+        sexLabel = new JLabel("Sex");
+        gbc.gridy++;
+        gbc.gridy++;
+        panel.add(sexLabel,gbc);
+
+        ageLabel = new JLabel("Age");
+        gbc.gridy++;
+        gbc.gridy++;
+        panel.add(ageLabel,gbc);
+
+
+        contactLabel = new JLabel("Contact No.");
+        gbc.gridy++;
+        gbc.gridy++;
+        panel.add(contactLabel,gbc);
+
+        courseLabel = new JLabel("Course");
+        gbc.gridy++;
+        gbc.gridy++;
+        panel.add(courseLabel,gbc);
+
+
         name=new JTextField(30);
-        gbc.gridx=2;
-        gbc.gridy=1;
+        gbc.gridx = 2;
+        gbc.gridy = 0;
+        gbc.ipady = 10;
         panel.add(name,gbc);
 
-        titleLabel=new JLabel("Address");
-        titleLabel.setSize(400,300);
-        gbc.gridx=4;
-        gbc.gridy=1;
-        panel.add(titleLabel,gbc);
         address=new JTextField(30);
-        gbc.gridx=2;
-        gbc.gridy=1;
+        gbc.gridy++;
+        gbc.gridy++;
         panel.add(address,gbc);
 
-
-        titleLabel=new JLabel("Age");
-        titleLabel.setSize(300,300);
-        gbc.gridx=5;
-        gbc.gridy=1;
-        panel.add(titleLabel,gbc);
-        age=new JTextField(30);
-        gbc.gridx=2;
-        gbc.gridy=1;
-        panel.add(age,gbc);
-
-        titleLabel=new JLabel("Sex");
-        titleLabel.setSize(300,300);
-        gbc.gridx=5;
-        gbc.gridy=1;
-        panel.add(titleLabel,gbc);
         sex=new JTextField(30);
-        gbc.gridx=2;
-        gbc.gridy=1;
+        gbc.gridy++;
+        gbc.gridy++;
         panel.add(sex,gbc);
 
-        titleLabel=new JLabel("Contact No.");
-        titleLabel.setSize(300,300);
-        gbc.gridx=5;
-        gbc.gridy=1;
-        panel.add(titleLabel,gbc);
+        age=new JTextField(30);
+        gbc.gridy++;
+        gbc.gridy++;
+        panel.add(age,gbc);
+
         contactno=new JTextField(30);
-        gbc.gridx=2;
-        gbc.gridy=1;
+        gbc.gridy++;
+        gbc.gridy++;
         panel.add(contactno,gbc);
 
-        titleLabel=new JLabel(" Course Of Study");
-        titleLabel.setSize(300,300);
-        gbc.gridx=5;
-        gbc.gridy=1;
-        panel.add(titleLabel,gbc);
         course=new JTextField(30);
-        gbc.gridx=2;
-        gbc.gridy=1;
+        gbc.gridy++;
+        gbc.gridy++;
         panel.add(course,gbc);
+
+        submitButton = new JButton("Submit");
+        gbc.gridy++;
+        gbc.gridy++;
+        gbc.gridy++;
+        panel.add(submitButton);
+
+
 
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
