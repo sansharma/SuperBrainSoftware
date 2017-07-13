668c719314cac09bb172fc679bc95d707023c2b7
PraneshC <praneshchettri28@gmail.com>
2017-07-13 08:27:59 +0545

    CourseDetails Class Edited

--- SuperBrain/src/Admin.java
+++ SuperBrain/src/Admin.java
@@ -7,117 +7,89 @@ import java.awt.event.ActionListener;
  * Created by aaaaa on 7/12/2017.
  */
 public class Admin extends JFrame {
-    private JPanel panel=new JPanel();
-    private JButton AddCoursebutton= new JButton("addcourse");
-   private JButton AddUserbutton=new JButton("adduser");
-   private JButton EnrollStudentbutton;
+    private JPanel panel;
+    private JButton AddCoursebutton;
+    private JButton EnrollStudentbutton;
+    private JButton CancelInstallmentbutton;
+    private  JButton ViewStudentDetailsbutton;
+    private JButton InstallmenPaymenttbutton;
+    private JButton AddUserbutton;
     private JButton DeleteStudentbutton;
     private JButton UpdateUserbutton;
-    private  JButton ViewStudentDetailsbutton;
     private JButton DeleteCoursebutton;
-    private JButton Installmentbutton;
     private JButton ViewNormalUsersbutton;
-    private JButton CancelInstallmentbutton;
+
 
     public Admin (){
+        panel = new JPanel();
         GridBagLayout gridBagLayout=new GridBagLayout();
-
         GridBagConstraints gbc =new GridBagConstraints();
         panel.setLayout(gridBagLayout);
+
+
         AddCoursebutton=new JButton("ADD COURSE");
-        gbc.gridx=2;
-        gbc.gridy=1;
-        gbc.ipadx = 5;
-        gbc.ipady = 2;
+        gbc.gridx=0;
+        gbc.gridy=0;
         gbc.gridwidth = 4;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
         panel.add(AddCoursebutton,gbc);
+
+        EnrollStudentbutton=new JButton("ENROLL STUDENT");
+        gbc.gridy++;
+        gbc.gridy++;
+        panel.add(EnrollStudentbutton,gbc);
+
+        CancelInstallmentbutton=new JButton("CANCEL INSTALLMENT");
+        gbc.gridy++;
+        gbc.gridy++;
+        panel.add(CancelInstallmentbutton,gbc);
+
+        ViewStudentDetailsbutton=new JButton("VIEW STUDENT DETAILS");
+        gbc.gridy++;
+        gbc.gridy++;
+        panel.add(ViewStudentDetailsbutton,gbc);
+
+        InstallmenPaymenttbutton=new JButton("INSTALLMENT PAYMENT");
+        gbc.gridy++;
+        gbc.gridy++;
+        panel.add(InstallmenPaymenttbutton,gbc);
+
+
 
         AddUserbutton=new JButton("ADD USER");
-        gbc.gridx=4;
-        gbc.gridy=1;
-        gbc.ipadx = 5;
-        gbc.ipady = 2;
-        gbc.gridwidth = 4;
-
+        gbc.gridy = 1;
+        gbc.gridy = 0;
+        gbc.gridwidth = 2;
+        gbc.ipadx = 6;
+        gbc.ipady = 3;
         panel.add(AddCoursebutton,gbc);
-        AddUserbutton=new JButton("ADD COURSE");
-        gbc.gridx=2;
-        gbc.gridy=2;
-        gbc.ipadx = 5;
-        gbc.ipady = 2;
-        gbc.gridwidth = 4;
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        panel.add(AddUserbutton,gbc);
-        EnrollStudentbutton=new JButton("ENROLL STUDENT");
-        gbc.gridx=4;
-        gbc.gridy=2;
-        gbc.ipadx = 5;
-        gbc.ipady = 2;
-        gbc.gridwidth = 4;
 
-        panel.add(EnrollStudentbutton,gbc);
         DeleteStudentbutton=new JButton("DELETE STUDENT");
-        gbc.gridx=3;
-        gbc.gridy=3;
-        gbc.ipadx = 5;
-        gbc.ipady = 2;
-        gbc.gridwidth = 4;
-
+        gbc.gridy++;
+        gbc.gridy++;
         panel.add(DeleteStudentbutton,gbc);
+
         UpdateUserbutton=new JButton("UPDATE USER");
-        gbc.gridx=5;
-        gbc.gridy=3;
-        gbc.ipadx = 5;
-        gbc.ipady = 2;
-        gbc.gridwidth = 4;
-        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.gridy++;
+        gbc.gridy++;
         panel.add(UpdateUserbutton,gbc);
-        ViewStudentDetailsbutton=new JButton("VIEW STUDENT DETAILS");
-        gbc.gridx=4;
-        gbc.gridy=4;
-        gbc.ipadx = 5;
-        gbc.ipady = 2;
-        gbc.gridwidth = 4;
 
-        panel.add(ViewStudentDetailsbutton,gbc);
         DeleteCoursebutton=new JButton("DELETE COURSE");
-        gbc.gridx=6;
-        gbc.gridy=4;
-        gbc.ipadx = 5;
-        gbc.ipady = 2;
-        gbc.gridwidth = 4;
-
+        gbc.gridy++;
+        gbc.gridy++;
         panel.add(DeleteStudentbutton,gbc);
-        Installmentbutton=new JButton("INSTALLMENT");
-        gbc.gridx=5;
-        gbc.gridy=5;
-        gbc.ipadx = 5;
-        gbc.ipady = 2;
-        gbc.gridwidth = 4;
 
-        panel.add(Installmentbutton,gbc);
         ViewNormalUsersbutton=new JButton("VIEW NORMAL USER");
-        gbc.gridx=7;
-        gbc.gridy=5;
-        gbc.ipadx = 5;
-        gbc.ipady = 2;
-        gbc.gridwidth = 4;
-
+        gbc.gridy++;
+        gbc.gridy++;
         panel.add(ViewStudentDetailsbutton,gbc);
-        CancelInstallmentbutton=new JButton("CANCEL INSTALLMENT");
-        gbc.gridx=6;
-        gbc.gridy=6;
-        gbc.ipadx = 5;
-        gbc.ipady = 2;
-        gbc.gridwidth = 4;
 
-        panel.add(CancelInstallmentbutton,gbc);
-        add(panel);
+
         setVisible(true);
         setSize(300,300);
-
+        add(panel);
     }
 
       public static void main(String args []){
-      new Admin();
+        new Admin();
       }}
